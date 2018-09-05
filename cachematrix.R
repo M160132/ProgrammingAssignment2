22d0e5061b3dcddb8ca272b7c304b085e7292991
M160132 <40144038+M160132@users.noreply.github.com>
2018-09-05 23:02:51 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,11 @@
 ## The main idea of this function is copied from rdpeng in his script of manipulation of mean
 
+# Calculating the inverse of a matrix is quite troublesome especially the order is very large. 
+# My functions below can effectively solve this problem. We input the matrix that we would like 
+# to compute its inverse in makeCacheMatrix. Then this function will generate a list containing 4 functions regarding the matrix
+# This is to prepare for the second function.
+
+
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -12,6 +18,9 @@ makeCacheMatrix <- function(x = matrix()) {
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+# We input the returned list generated in function makeCacheMatrix in the function below cacheSolve.
+# If the returned list containing the inverse of the original matrix, then the cacheSolve will directly extract the inverse matrix
+# Otherwise, the inverse will be calculated here.
 
 cacheSolve <- function(x, ...) {
         inv <- x$getInverse()
